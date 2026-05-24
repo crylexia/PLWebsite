3ee10bad1ff34527666466ec134c5348f36b71c4
kael <chaeilmeneses2@gmail.com>
2026-05-24 11:05:20 +0800

    Fix mailer to use  instead of getenv for SMTP credentials

--- config/mailer.php
+++ config/mailer.php
@@ -20,13 +20,13 @@ function sendMail($to, $subject, $body)
     $mail = new PHPMailer(true);
     try {
         $mail->isSMTP();
-        $mail->Host       = getenv('MAIL_HOST');
+        $mail->Host       = $_ENV['MAIL_HOST'];
         $mail->SMTPAuth   = true;
-        $mail->Username   = getenv('MAIL_USERNAME');
-        $mail->Password   = getenv('MAIL_PASSWORD');
+        $mail->Username   = $_ENV['MAIL_USERNAME'];
+        $mail->Password   = $_ENV['MAIL_PASSWORD'];
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-        $mail->Port       = getenv('MAIL_PORT');
-        $mail->setFrom(getenv('MAIL_FROM'), getenv('MAIL_FROM_NAME'));
+        $mail->Port       = $_ENV['MAIL_PORT'];
+        $mail->setFrom($_ENV['MAIL_FROM'], $_ENV['MAIL_FROM_NAME']);
         $mail->addAddress($to);
         $mail->isHTML(true);
         $mail->Subject = $subject;
